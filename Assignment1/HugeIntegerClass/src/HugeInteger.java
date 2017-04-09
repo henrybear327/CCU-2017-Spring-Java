f25f43671347985a86da0c1b9833f2836cb15675
Henry Tseng <henrybear327@gmail.com>
2017-04-09 15:58:36 +0800

    Implemented sub and mul

--- Assignment1/HugeIntegerClass/src/HugeInteger.java
+++ Assignment1/HugeIntegerClass/src/HugeInteger.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import sun.tools.jar.resources.jar;
+
 // click on "default package" and click the "play button" to run the program
 
 // Questions:
@@ -7,9 +9,9 @@ import java.util.Arrays;
 // 2. public boolean isEqualTo(HugeInteger other) or public boolean isEqualTo(HugeInteger a, HugeInteger b)
 // 3. Handle negative number, overflow?
 
-public class HugeInteger{
+public class HugeInteger {
 	public int[] numberArray;
-	
+
 	final int ARRAY_SIZE = 40;
 
 	public HugeInteger() {
@@ -27,61 +29,70 @@ public class HugeInteger{
 		}
 
 		Arrays.fill(this.numberArray, 0);
-		
+
 		for (int i = inp.length() - 1; i >= 0; i--) {
 			numberArray[inp.length() - 1 - i] = this.charToInt(inp.charAt(i));
 		}
-		
+
 		/*
-		for (int i = ARRAY_SIZE - 1; i >= 0; i--)
-			System.out.printf("%3d%c", i, i == 0 ? '\n' : ' ');
-		for (int i = ARRAY_SIZE - 1; i >= 0; i--)
-			System.out.printf("%3d%c", numberArray[i], i == 0 ? '\n' : ' ');
-		*/
+		 * for (int i = ARRAY_SIZE - 1; i >= 0; i--) System.out.printf("%3d%c",
+		 * i, i == 0 ? '\n' : ' '); for (int i = ARRAY_SIZE - 1; i >= 0; i--)
+		 * System.out.printf("%3d%c", numberArray[i], i == 0 ? '\n' : ' ');
+		 */
 	}
 
 	public String toString() {
 		String result = "";
-		
+
 		int i;
 		for (i = ARRAY_SIZE - 1; i >= 0; i--)
-			if(numberArray[i] != 0) 
+			if (numberArray[i] != 0)
 				break;
-		
-		for(; i >= 0; i--)
+
+		for (; i >= 0; i--)
 			result += String.valueOf(numberArray[i]);
-		
+
 		return result.length() == 0 ? "0" : result;
 	}
 
 	public HugeInteger add(HugeInteger other) {
 		HugeInteger result = new HugeInteger();
-		for(int i = 0; i < ARRAY_SIZE; i++) {
+		for (int i = 0; i < ARRAY_SIZE; i++) {
 			result.numberArray[i] += this.numberArray[i] + other.numberArray[i];
-			if(i + 1 < ARRAY_SIZE) {
+			if (i + 1 < ARRAY_SIZE) {
 				result.numberArray[i + 1] = result.numberArray[i] / 10;
 				result.numberArray[i] %= 10;
 			}
 		}
-		
+
 		return result;
 	}
 
 	public HugeInteger subtract(HugeInteger other) {
 		HugeInteger result = new HugeInteger();
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			if (this.numberArray[i] < other.numberArray[i]) {
+				if (i + 1 < ARRAY_SIZE)
+					this.numberArray[i + 1]--;
+				result.numberArray[i] = this.numberArray[i] - other.numberArray[i] + 10;
+			} else
+				result.numberArray[i] = this.numberArray[i] - other.numberArray[i];
 		}
-		
+
 		return result;
 	}
-	
+
 	public HugeInteger multiply(HugeInteger other) {
 		HugeInteger result = new HugeInteger();
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			
+		for (int j = 0; j < ARRAY_SIZE; j++) {
+			for (int i = 0; i + j < ARRAY_SIZE; i++) {
+				result.numberArray[i + j] += this.numberArray[i] * other.numberArray[j];
+				if (i + j + 1 < ARRAY_SIZE)
+					result.numberArray[i + j + 1] += result.numberArray[i + j] / 10;
+				result.numberArray[i + j] %= 10;
+			}
 		}
-		
+
 		return result;
 	}
 	
@@ -89,56 +100,56 @@ public class HugeInteger{
 		if (other.isZero()) {
 			throw new RuntimeException("Divide by zero is not acceptable");
 		}
-		
+
 		HugeInteger result = new HugeInteger();
-		for(int i = 0; i < ARRAY_SIZE; i++) {
+		for (int i = 0; i < ARRAY_SIZE; i++) {
 			
 		}
-		
+
 		return result;
 	}
-	
+
 	public HugeInteger remainder(HugeInteger other) {
 		if (other.isZero()) {
 			throw new RuntimeException("Divide by zero is not acceptable");
 		}
-		
+
 		HugeInteger result = new HugeInteger();
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+
 		}
-		
+
 		return result;
 	}
-	
+
 	public boolean isEqualTo(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isNotEqualTo(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isGreaterThan(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isLessThan(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isGreaterThanOrEqualTo(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isLessThanOrEqualTo(HugeInteger other) {
 		return false;
 	}
-	
+
 	public boolean isZero() {
 		int count = 0;
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			if(this.numberArray[i] == 0)
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			if (this.numberArray[i] == 0)
 				count++;
 		}
 		return count == ARRAY_SIZE;
