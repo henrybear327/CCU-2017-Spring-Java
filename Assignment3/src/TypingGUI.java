1abd901b6767445eb3080b56bbbe3d27e3a3cea9
Henry Tseng <henrybear327@gmail.com>
2017-05-22 10:26:46 +0800

    basic UI -- done

--- Assignment3/src/TypingGUI.java
+++ Assignment3/src/TypingGUI.java
@@ -7,81 +7,166 @@ import java.awt.event.ActionListener;
  * Created by henrybear327 on 5/19/17.
  */
 public class TypingGUI {
+
+    private Container container;
     private JFrame frame;
     private JPanel panel;
+
+    private JLabel scoreLabel;
 
     private final int windowWidth = 800;
     private final int windowHeight = 600;
 
     public TypingGUI() {
-        configureFrame();
+//        testBoxLayout();
 
-        configureLayout();
+        configure();
 
         configureInstructionLabel();
 
-        configureTextArea();
+        addEmptyLabel();
 
+        configureShowScore();
+
+        addEmptyLabel();
+
+        configurePrintTextArea();
+
+        addEmptyLabel();
+
+        configureGetTextArea();
+
+        addEmptyLabel();
+
+        configureKeyboard();
+
+        container.add(panel);
         frame.setVisible(true);
     }
 
-    private void configureFrame() {
+    private void configure() {
         frame = new JFrame();
         frame.setTitle("Typing Application");
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(windowWidth, windowHeight);
 
-        frame.setResizable(false);
+        frame.setLayout(new GridLayout(1,1));
+
+//        frame.setResizable(false);
+
+        container = frame.getContentPane();
+        container.setLayout(new GridLayout(0, 1));
 
         panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     }
 
-    private void configureLayout() {
-        frame.setContentPane(panel);
+    private void addEmptyLabel() {
+        JPanel emptyLabelPanel = new JPanel();
+
+        JLabel emptyLabel = new JLabel();
+        emptyLabel.setText("<html><body><br></body></html>");
+        emptyLabelPanel.add(emptyLabel);
+
+        emptyLabel.setPreferredSize(new Dimension(windowWidth - 20, 10));
+        emptyLabel.setMaximumSize(new Dimension(windowWidth - 20, 10));
+
+        emptyLabelPanel.setPreferredSize(new Dimension(windowWidth, 10));
+        emptyLabelPanel.setMaximumSize(new Dimension(windowWidth, 10));
+
+        panel.add(emptyLabelPanel);
     }
 
     private void configureInstructionLabel() {
         JPanel instructionLabelPanel = new JPanel();
 
-        instructionLabelPanel.add(Box.createVerticalStrut(10));
-
         JLabel instructionLabel = new JLabel();
         instructionLabel.setText("<html><body>Type some text using your keyboard. The keys you press will be highlighted and the text will be displayed.<br>Note: Clicking the buttons with your mouse will not perform any action.</body></html>");
         instructionLabelPanel.add(instructionLabel);
 
-        instructionLabelPanel.add(Box.createVerticalStrut(10));
+        instructionLabel.setPreferredSize(new Dimension(windowWidth - 20, 40));
+        instructionLabel.setMaximumSize(new Dimension(windowWidth - 20, 40));
 
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        c.fill = GridBagConstraints.BOTH ;
-        panel.add(instructionLabelPanel, c);
+        instructionLabelPanel.setPreferredSize(new Dimension(windowWidth, 40));
+        instructionLabelPanel.setMaximumSize(new Dimension(windowWidth, 40));
+
+        panel.add(instructionLabelPanel);
     }
 
-    private void configureTextArea() {
-        JPanel textAreaPanel = new JPanel();
-        
-        textAreaPanel.add(Box.createVerticalStrut(10));
+    private void configureShowScore() {
+        scoreLabel = new JLabel();
+        scoreLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        scoreLabel.setText("<html><body>Score label... TODO</body></html>");
 
-        JTextArea textArea = new JTextArea(100, 100);
-        textArea.setText("");
+        panel.add(scoreLabel);
+    }
+
+    private void configurePrintTextArea() {
+        JPanel textAreaPanel = new JPanel();
+        textAreaPanel.setLayout(new GridLayout(1,1));
+
+        JTextArea textArea = new JTextArea("1");
         textArea.setEnabled(false);
         textArea.setDisabledTextColor(Color.BLACK);
 
-        textAreaPanel.add(Box.createVerticalStrut(10));
+//        textArea.setPreferredSize(new Dimension(windowWidth - 20, 40));
+//        textArea.setMaximumSize(new Dimension(windowWidth - 20, 40));
+//
+//        textAreaPanel.setPreferredSize(new Dimension(windowWidth, 60));
+//        textAreaPanel.setMaximumSize(new Dimension(windowWidth, 60));
 
         textAreaPanel.add(textArea);
 
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 1;
-        c.weightx = 1.0;
-        c.weighty = 1.0;
-        c.fill = GridBagConstraints.BOTH ;
-        panel.add(textAreaPanel, c);
+        panel.add(textAreaPanel);
+    }
+
+    private void configureGetTextArea() {
+        JPanel textAreaPanel = new JPanel();
+        textAreaPanel.setLayout(new GridLayout(1,1));
+
+        JTextArea textArea = new JTextArea("2");
+        textArea.setEnabled(false);
+        textArea.setDisabledTextColor(Color.BLACK);
+
+//        textArea.setPreferredSize(new Dimension(windowWidth - 20, 40));
+//        textArea.setMaximumSize(new Dimension(windowWidth - 20, 40));
+//
+//        textAreaPanel.setPreferredSize(new Dimension(windowWidth, 60));
+//        textAreaPanel.setMaximumSize(new Dimension(windowWidth, 60));
+
+        textAreaPanel.add(textArea);
+
+        panel.add(textAreaPanel);
+    }
+
+    private void configureKeyboard() {
+
+    }
+
+    private void testBoxLayout() {
+        // http://progden-programming.blogspot.tw/2013/02/layout-boxlayout.html
+        JFrame frame = new JFrame("BoxLayout-1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container cp = frame.getContentPane();
+        cp.setLayout(new GridLayout(0, 1));
+
+        JPanel panel = null;
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        cp.add(panel);
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        cp.add(panel);
+
+        frame.setVisible(true);
+        frame.pack();
     }
 }
